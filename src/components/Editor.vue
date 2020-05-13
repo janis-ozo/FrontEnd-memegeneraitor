2017576d46013ff1f7d339c884a1c53f8a3b727b
janis-ozo <janis_O@hotmail.com>
2020-05-13 10:24:22 +0300

    add download

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -2,48 +2,52 @@
   <div class="editor">
     <p class="my-4">Hello from editor! {{ imgToEdit }}</p>
     <b-form-input v-model="text" placeholder="Enter your TEXT"></b-form-input>
-    <div class="image-and-text">
-      <b-img
-        thumbnail
-        fluid
-        :src="imgToEdit"
-        left
-        :alt="`Image to edit`"
-        class="image"
-      ></b-img>
-      <div class="textonimage">{{ text }}</div>
+    <div id="download" ref="download">
+      <div class="image-and-text">
+        <b-img
+          thumbnail
+          fluid
+          :src="imgToEdit"
+          left
+          :alt="`Image to edit`"
+          class="image"
+        ></b-img>
+        <div class="textonimage">{{ text }}</div>
+      </div>
     </div>
     <label for="range-1">Text size</label>
     <div class="mt-2">Value: {{ textSize }}</div>
     <b-form-input
       id="range-1"
-      v-model="value"
+      v-model="textSize"
       type="range"
       min="0"
       max="100"
     ></b-form-input>
     <label for="range-1">Position X</label>
-    <div class="mt-2">Value: {{ x }}</div>
+    <div class="mt-2">Value: {{ textX }}</div>
     <b-form-input
       id="range-1"
-      v-model="value"
+      v-model="textX"
       type="range"
       min="0"
       max="100"
     ></b-form-input>
     <label for="range-1">Position Y</label>
-    <div class="mt-2">Value: {{ y }}</div>
+    <div class="mt-2">Value: {{ textY }}</div>
     <b-form-input
       id="range-1"
-      v-model="value"
+      v-model="textY"
       type="range"
       min="0"
       max="100"
     ></b-form-input>
+    <b-button @click="download" variant="success">Download</b-button>
   </div>
 </template>
 
 <script>
+import { saveAsPng } from 'save-html-as-image';
 export default {
   name: "Gallery",
   props: {
@@ -54,7 +58,7 @@ export default {
   data() {
     return {
       text: "",
-      textSize: 10,
+      textSize: 100,
       width: 0,
       height: 0,
       textX: 50,
@@ -62,14 +66,14 @@ export default {
     };
   },
   methods: {
-    // onResize: function(x, y, width, height) {
-    //   this.x = x;
-    //   this.y = y;
-    //   this.width = width;
-    //   this.height = height;
-    // },
-    resizeTextX: function(textX) {
-      this.textX = textX;
+    download() {
+      const node = this.$refs.download
+      saveAsPng(node, {filename:'meme', printDate:false})
+    },
+    resizeText: function() {
+      return {
+        fontSize: this.textSize + 'px'
+      }
     }
     // onDrag: function(x, y) {
     //   this.x = x;
@@ -83,9 +87,11 @@ export default {
 <style scoped>
 .image-and-text {
   position: relative;
+
 }
 
 .textonimage {
   position: absolute;
+  font-size: 50px;
 }
 </style>
